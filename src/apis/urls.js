40751913b63feb3975d3f6e01185bc7abd798d72
Avain <sports113499@gmail.com>
2024-04-21 00:59:01 +0800

    feat: add test api in todopage

--- src/apis/urls.js
+++ src/apis/urls.js
@@ -3,5 +3,7 @@ const baseURL = 'https://todoo.5xcamp.us';
 const signupAPI = baseURL + '/users';
 const signinAPI = baseURL + '/users/sign_in';
 const signoutAPI = baseURL + '/users/sign_out';
+const testAPI = baseURL + '/check';
 
-export { signupAPI,signinAPI,signoutAPI };
+
+export { signupAPI,signinAPI,signoutAPI,testAPI };
--- src/components/TodoPage.jsx
+++ src/components/TodoPage.jsx
@@ -1,15 +1,38 @@
-// import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import Header from "./Header";
 import AddList from "./AddList";
 import TodoList from "./TodoList";
+import { testAPI } from "../apis/urls";
 
 function TodoPage() {
+  const navigate = useNavigate();
+  async function checkToken(){
+    const token = window.localStorage.getItem('token');
+    const config = {
+      headers:{ authorization: token }
+    };
+    try {
+      const response = await axios.get(testAPI,config);
+      console.log(response);
+      alert("歡迎回來");
+    } catch (error) {
+      alert(error.response.data.message);
+      window.localStorage.clear();
+      navigate('/todolist-react/');
+    }
+  }
+  
+  useEffect(()=>{
+    checkToken();
+  },[]);
   return (
     <>
-      <Header></Header>
+      <Header />
       <main>
-        <AddList></AddList>
-        <TodoList></TodoList>
+        <AddList />
+        <TodoList />
       </main>
     </>
   );
